3bf555448a452552ad7edac1b194ef4f4ec207fd
Yxleak <sinosbro2@gmail.com>
2024-02-13 10:38:50 -0500

    Working On Camera/Jump
    2/13/2024

--- GameDev2-FPS/Assets/Player/PlayerController.cs
+++ GameDev2-FPS/Assets/Player/PlayerController.cs
@@ -6,77 +6,70 @@ using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
-    // Debug
-    public TMP_Text debug_text;
+    //Debug
+    //public TMP_Text debug_text;
 
-    // Camera Variables
+    //Camera Variables
     public Camera cam;
     private Vector2 look_input = Vector2.zero;
-    private float look_speed = 60f;
-    private float horizontal_look_angle = 0f;
+    private float look_speed = 60;
+    private float horizontal_look_angle = 0;
     public bool invert_x = false;
-    public int invert_factor_x = 1;
     public bool invert_y = false;
-    public int invert_factor_y = 1;
-    [Range(0.01f, 1f)]public float sensitivity;
+    private int invert_factor_x = 1;
+    private int invert_factor_y = 1;
+    [Range(0.01f, 1f)] public float sensitivity;
 
-    // Player Inputs
-    private Vector2 move_input;
+    //Player Input
+    /*private Vector2 move_input;
     private bool grounded;
 
-    // Movement Variables
-    private CharacterController characterController;
+    //Movement Variables
+    private CharacterController character_controller;
     private Vector3 player_velocity;
     private Vector3 wish_dir = Vector3.zero;
-    public float max_speed = 6f;
-    public float acceleration = 60f;
-    public float gravity = 15f;
+    public float max_speed = 6;
+    public float acceleration = 60;
+    public float gravity = 20;
     public float stop_speed = 0.5f;
     public float jump_impulse = 10f;
-    public float friction = 4f;
-
-
+    public float friction = 4;*/
 
     // Start is called before the first frame update
     void Start()
     {
-        // Hide Mouse
+        //Hide the mouse.
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
 
-        // Invert Camera T_T (why?????)
+        //Invert Camera
         if (invert_x) invert_factor_x = -1;
         if (invert_y) invert_factor_y = -1;
 
-        // Get reference to character controller component.
-        characterController = GetComponent<CharacterController>();
-
+        //Get Components
+        //character_controller = GetComponent<CharacterController>();
     }
-
-
 
     // Update is called once per frame
     void Update()
     {
-        // Debug
-        debug_text.text = "Wish Dir: " + wish_dir.ToString();
-        debug_text.text += "\nVelocity: " + player_velocity.ToString();
-        debug_text.text += "\nSpeed: " + new Vector3(player_velocity.x, 0, player_velocity.z).magnitude.ToString();
-        debug_text.text += "\nGround: " + grounded.ToString();
+        //Debug
+        /*debug_text.text = "Wish Dir: " + wish_dir.ToString();
+        debug_text.text += "\nPlayer Velocity: " + player_velocity.ToString();
+        debug_text.text += "\nPlayey Speed: " + new Vector3(player_velocity.x, 0, player_velocity.z).magnitude.ToString();
+        debug_text.text += "\nGrounded: " + grounded.ToString();*/
 
         Look();
     }
 
-
-
-    private void FixedUpdate()
+    /*private void FixedUpdate()
     {
-        // Find the wish_dir.
+        //Find wish dir
         wish_dir = transform.right * move_input.x + transform.forward * move_input.y;
         wish_dir = wish_dir.normalized;
 
-        // To check if player is grounded
-        if(grounded)
+        grounded = character_controller.isGrounded;
+        if (grounded)
         {
             player_velocity = MoveGround(wish_dir, player_velocity);
         }
@@ -85,23 +78,23 @@ public class PlayerController : MonoBehaviour
             player_velocity = MoveAir(wish_dir, player_velocity);
         }
 
-        // Gravity
+        //Gravity
         player_velocity.y -= gravity * Time.deltaTime;
-        if(grounded && player_velocity.y < 0)
+        if (grounded && player_velocity.y < 0) //Cap Y velocity on ground.
         {
             player_velocity.y = -2;
         }
-        characterController.Move(player_velocity * Time.deltaTime);
-    }
 
-
+        //Move the player.
+        character_controller.Move(player_velocity * Time.deltaTime);
+    }*/
 
     public void GetLookInput(InputAction.CallbackContext context)
     {
         look_input = context.ReadValue<Vector2>();
     }
 
-    public void GetMoveInput(InputAction.CallbackContext context)
+    /*public void GetMoveInput(InputAction.CallbackContext context)
     {
         move_input = context.ReadValue<Vector2>();
     }
@@ -109,70 +102,62 @@ public class PlayerController : MonoBehaviour
     public void GetJumpInput(InputAction.CallbackContext context)
     {
         Jump();
-    }
-
-
+    }*/
 
     private void Look()
     {
-        // Left and Right Look
+        //Left/Right 
         transform.Rotate(Vector3.up, look_input.x * look_speed * Time.deltaTime * invert_factor_x * sensitivity);
 
-        // Up and Down Look
+        //Up/Down
         float angle = look_input.y * look_speed * Time.deltaTime * invert_factor_y * sensitivity;
         horizontal_look_angle -= angle;
         horizontal_look_angle = Mathf.Clamp(horizontal_look_angle, -90, 90);
         cam.transform.localRotation = Quaternion.Euler(horizontal_look_angle, 0, 0);
     }
 
-    public void Jump()
+    /*private void Jump()
     {
-        if(grounded)
+        if (grounded)
         {
             player_velocity.y = jump_impulse;
         }
     }
 
-
-
     private Vector3 Accelerate(Vector3 wish_dir, Vector3 current_velocity, float accel, float max_speed)
     {
-        // Projects speed in which player goes.
-        float proj_Speed = Vector3.Dot(current_velocity, wish_dir);
+        //Project current_velocity on to the wish_dir
+        float proj_speed = Vector3.Dot(current_velocity, wish_dir);
         float accel_speed = accel * Time.deltaTime;
 
-        // Truncate accelerated velocity if needed.
-        if(proj_Speed + accel_speed > max_speed)
-        {
-            accel_speed = max_speed - proj_Speed;
-        }
+        if (proj_speed + accel_speed > max_speed)
+            accel_speed = max_speed - proj_speed;
 
-        // Return new speed.
         return current_velocity + (wish_dir * accel_speed);
     }
 
-    private Vector3 MoveAir(Vector3 wish_dur, Vector3 current_velocity)
+    private Vector3 MoveGround(Vector3 wish_dir, Vector3 current_velocity)
     {
-        return Accelerate(wish_dir, current_velocity, acceleration, max_speed);
-    }
-    private Vector3 MoveGround(Vector3 wish_dur, Vector3 current_velocity)
-    {
-        Vector3 new_velocity = new Vector3(current_velocity.x, 0, current_velocity.z); //Remove y velocity component
+        Vector3 new_velocity = new Vector3(current_velocity.x, 0, current_velocity.z);
 
         float speed = new_velocity.magnitude;
-        if(speed <= stop_speed)
+        if (speed <= stop_speed)
         {
             new_velocity = Vector3.zero;
             speed = 0;
         }
-
-        if(speed != 0)
+        if (speed != 0)
         {
             float drop = speed * friction * Time.deltaTime;
-            new_velocity *= Mathf.Max(speed - drop, 0) / speed; //Scale velocity based on friction
+            new_velocity *= Mathf.Max(speed - drop, 0) / speed;
         }
-        new_velocity = new Vector3(new_velocity.x, current_velocity.y, current_velocity.z); //Add back y velocity component
+        new_velocity = new Vector3(new_velocity.x, current_velocity.y, new_velocity.z);
 
         return Accelerate(wish_dir, new_velocity, acceleration, max_speed);
     }
+
+    private Vector3 MoveAir(Vector3 wish_dir, Vector3 current_velocity)
+    {
+        return Accelerate(wish_dir, current_velocity, acceleration, max_speed);
+    }*/
 }
